3653a7aa1df522a7e6b53dcad0698221145b8ba6
jfelipesouza <joao.felipe.18.dev@gmail.com>
2023-04-27 23:52:22 -0300

    limpando projeto

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { app } from './services/config/server'
 
 const port = process.env.PORT || 3001
 
-const events = ['SIGINT', 'SIGTERM', 'SIGQUIT']
-
 app.listen(port, () => {
   console.log(`the server is online in port ${port}`)
 })
